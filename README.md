6636864388bfe634e4371116c709448391dd16fb
JeiversonChristian <jeiversonc@gmail.com>
2024-07-21 17:12:30 -0300

    coloquei tudo de uma vez

---
+++ README.md
@@ -0,0 +1,16 @@
+# Colisão de Células
+### Programa que anima duas células e mostra quando elas colidem
+
+## Problema:
+
+O programa acusa que elas se colidiram e mostra os valores das suas posições de acordo com o esperado, mas a imagem mostrada na tela não fica de acordo com o esperado.
+
+## Imagem explicativa
+
+![software_print](1.PNG)
+![software_print](2.PNG)
+![software_print](3.PNG)
+
+## Teste você mesmo no link:
+
+[Link da simulação](https://www.youtube.com/watch?v=Klp2Q462chU)
---
+++ main.js
@@ -0,0 +1,87 @@
+// Esta função é acionada ao carregar a página
+window.onload = function() {
+    //definindo_parametros ----------------------------------------------------------
+    window.canvas = document.getElementById('canvas');
+    window.ctx = canvas.getContext('2d');
+
+    window.canvas.width = 1000;
+    window.canvas.height = 500;
+
+    window.fps = 30;
+    window.interval = 1000/fps;
+    window.lastTime = 0;
+    // --------------------------------------------------------------------------------
+    
+    //criando celulas -----------------------------------------------------------------
+    //célula1
+    let c1r = 20 // raio da célula
+    let c1x = 0 + c1r // coordenada x do centro da célula
+    let c1y = window.canvas.height / 2 // coordenada y do centro da célula
+    let c1v = 5 // velocidade da célula
+    let c1c = 'blue' // cor da célula
+    //célula2
+    let c2r = 20 // raio da célula
+    let c2x = window.canvas.width - c2r // coordenada x do centro da célula
+    let c2y = window.canvas.height / 2 // coordenada y do centro da célula
+    let c2v = 5 // velocidade da célula
+    let c2c = 'red' // cor da célula
+    // --------------------------------------------------------------------------------
+
+    // --------------------------------------------------------------------------------
+    function desenhar_celulas(){
+        //célula1
+        ctx.beginPath();
+        ctx.arc(c1x, c1y, c1r, 0, Math.PI*2, false);
+        ctx.fillStyle = c1c;
+        ctx.fill();
+        ctx.stroke();
+        //célula2
+        ctx.beginPath();
+        ctx.arc(c2x, c2y, c2r, 0, Math.PI*2, false);
+        ctx.fillStyle = c2c;
+        ctx.fill();
+        ctx.stroke();
+    }
+    // --------------------------------------------------------------------------------
+
+    // --------------------------------------------------------------------------------
+    function atualizar_posicoes(){
+        //célula1
+        c1x += c1v
+        //célula2
+        c2x -= c2v
+    }
+    // --------------------------------------------------------------------------------
+
+    // --------------------------------------------------------------------------------
+    function limpar_tela(){
+        ctx.clearRect(0, 0, window.canvas.width, window.canvas.height);
+    }
+    // --------------------------------------------------------------------------------
+
+    // --------------------------------------------------------------------------------
+    function verificar_colisao(){
+        if ((c1x + c1r) >= (c2x - c2r)) {
+            let mensagem = "c1x: "+String(c1x) +" | Borda de c1: "+String(c1x+c1r)+"\n"+"c2x: "+String(c2x) +" | Borda de c2: "+String(c2x-c2r)
+            window.alert(mensagem)
+        }
+    }
+    // --------------------------------------------------------------------------------
+
+    // --------------------------------------------------------------------------------
+    // obs: timestamp já é passado automaticamente pela função requestAnimationFrame
+    function rodar_jogo(timestamp){
+        if (timestamp - window.lastTime >= window.interval) {
+            window.lastTime = timestamp;
+            atualizar_posicoes();
+            limpar_tela();
+            desenhar_celulas();
+            verificar_colisao();
+        }
+        requestAnimationFrame(rodar_jogo);
+    }
+    // --------------------------------------------------------------------------------
+
+    desenhar_celulas();
+    requestAnimationFrame(rodar_jogo);
+}
